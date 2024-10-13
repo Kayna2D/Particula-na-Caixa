3f2bf50bd87a63c684f5d49a2482e5ff932ce608
Kayna2D <unifkaysilva@fei.edu.br>
2024-10-12 23:50:43 -0300

    funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,6 +1,10 @@
 from math import *
 from  decimal import Decimal
 
+h = 6.626E-34
+c = 3E8
+h_ev = 4.136E-15
+
 me = 9.11E-31
 mp = 1.67E-27
 largura = float(input("Entre com a largura: "))
@@ -15,8 +19,28 @@ def funcOndaInicial(largura, ni):
 def funcOndaFinal(largura, nf): 
     return f'Funcao de onda do nivel final: {Decimal(sqrt(2/largura)):.3E} . sen({Decimal(nf*pi/largura):.3E} . x)'
 
-#print(f'Funcao de onda do nivel inicial: {Decimal(sqrt(2/largura)):.3E} . sen({Decimal(ni*pi/largura):.3E} . x)')
-#print(f'Funcao de onda do nivel final: {Decimal(sqrt(2/largura)):.3E} . sen({nf*pi/largura} . x)')
+def ei(ni):
+    return ni**2*h**2/(8*mp*largura**2)
+
+def ei_ev(ni):
+    return ei(ni)/1.602E-19
+
+def ef(nf):
+    return nf**2*h**2/(8*mp*largura**2)
+
+def ef_ev(nf):
+    return ef(nf)/1.602E-19
+
+def efoton(ni, nf):
+    efoton = ei_ev(ni) - ef_ev(nf)
+    if efoton < 0:
+        efoton = -efoton
+    return efoton
 
 print(funcOndaInicial(largura, ni))
 print(funcOndaFinal(largura, nf))
+print(f'Energia inicial: {Decimal(ei(ni)):.3E} J')
+print(f'Energia inicial: {Decimal(ei_ev(ni)):.3E} eV')
+print(f'Energia final: {Decimal(ef(nf)):.3E} J')
+print(f'Energia final: {Decimal(ef_ev(nf)):.3E} eV')
+print(f'Energia do foton: {Decimal(efoton(ni, nf)):.3E} eV')
