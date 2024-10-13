9c6f10b8cf083043fd08682e9808438f2a5d13fa
Kayna2D <unifkaysilva@fei.edu.br>
2024-10-13 03:26:16 -0300

    de broglie

--- funcoes.py
+++ funcoes.py
@@ -19,20 +19,14 @@ def funcOndaInicial(largura, ni):
 def funcOndaFinal(largura, nf): 
     return f'Funcao de onda do nivel final: {Decimal(sqrt(2/largura)):.3E} . sen({Decimal(nf*pi/largura):.3E} . x)'
 
-def ei(ni):
-    return ni**2*h**2/(8*mp*largura**2)
+def en(n, m, largura):
+    return n**2*h**2/(8*m*largura**2)
 
-def ei_ev(ni):
-    return ei(ni)/1.602E-19
+def en_ev(n, m, largura):
+    return en(n, m, largura)/1.602E-19
 
-def ef(nf):
-    return nf**2*h**2/(8*mp*largura**2)
-
-def ef_ev(nf):
-    return ef(nf)/1.602E-19
-
-def efoton(ni, nf):
-    efoton = ei_ev(ni) - ef_ev(nf)
+def efoton(ni, nf, m, largura):
+    efoton = en_ev(ni, m, largura) - en_ev(nf, m, largura)
     if efoton < 0:
         efoton = -efoton
     return efoton
@@ -43,20 +37,23 @@ def comprimento(efoton):
 def frequencia(efoton):
     return efoton/h*1.602E-19
 
-def velocidadeInicial(ni):
-    return sqrt(2*ei(ni)/mp)
+def velocidade(n, m, largura):
+    return sqrt(2*en(n, m, largura)/m)
 
-def velocidadeFinal(nf):
-    return sqrt(2*ei(nf)/mp)
+
+def deBroglie(m, v):
+    return h/(m*v)
 
 print(funcOndaInicial(largura, ni))
 print(funcOndaFinal(largura, nf))
-print(f'Energia inicial: {Decimal(ei(ni)):.3E} J')
-print(f'Energia inicial: {Decimal(ei_ev(ni)):.3E} eV')
-print(f'Energia final: {Decimal(ef(nf)):.3E} J')
-print(f'Energia final: {Decimal(ef_ev(nf)):.3E} eV')
-print(f'Energia do foton: {Decimal(efoton(ni, nf)):.3E} eV')
-print(f'Comprimento do foton: {Decimal(comprimento(efoton(ni, nf))):.3E} m')
-print(f'Frequencia do foton: {Decimal(frequencia(efoton(ni, nf))):.3E} Hz')
-print(f'Velocidade inicial: {Decimal(velocidadeInicial(ni)):.3E} m/s')
-print(f'Velocidade final: {Decimal(velocidadeFinal(nf)):.3E} m/s')
+print(f'Energia inicial: {Decimal(en(ni, mp, largura)):.3E} J')
+print(f'Energia inicial: {Decimal(en_ev(ni, mp, largura)):.3E} eV')
+print(f'Energia final: {Decimal(en(nf, mp, largura)):.3E} J')
+print(f'Energia final: {Decimal(en_ev(nf, mp, largura)):.3E} eV')
+print(f'Energia do foton: {Decimal(efoton(ni, nf, mp, largura)):.3E} eV')
+print(f'Comprimento do foton: {Decimal(comprimento(efoton(ni, nf, mp, largura))):.3E} m')
+print(f'Frequencia do foton: {Decimal(frequencia(efoton(ni, nf, mp, largura))):.3E} Hz')
+print(f'Velocidade inicial: {Decimal(velocidade(ni, mp, largura)):.3E} m/s')
+print(f'Velocidade final: {Decimal(velocidade(nf, mp, largura)):.3E} m/s')
+print(f'Comprimento  de onda de De Broglie inicial: {Decimal(deBroglie(mp, velocidade(ni, mp, largura))):.3E} m')
+print(f'Comprimento  de onda de De Broglie final: {Decimal(deBroglie(mp, velocidade(nf, mp, largura))):.3E} m')
