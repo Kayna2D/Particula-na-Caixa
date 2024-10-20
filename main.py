eff4bd1c67c6c7341d2949b702b9ab3e9ca6a0ad
Kayna2D <unifkaysilva@fei.edu.br>
2024-10-20 00:57:50 -0300

    titulo ad

--- main.py
+++ main.py
@@ -11,37 +11,39 @@ mp = 1.67E-27
 root = tk.Tk()
 root.title("Particula na Caixa")
 
-tk.Label(root, text="Largura (m):").grid(row=0, column=0, padx=10, pady=5)
+tk.Label(root, text="Partícula na Caixa", font=("Arial", 16, "bold")).grid(row=0, column=0, columnspan=2, pady=10)
+
+tk.Label(root, text="Largura (m):").grid(row=1, column=0, padx=10, pady=5)
 entry_largura = tk.Entry(root)
-entry_largura.grid(row=0, column=1)
+entry_largura.grid(row=1, column=1)
 
-tk.Label(root, text="Nível inicial:").grid(row=1, column=0, padx=10, pady=5)
+tk.Label(root, text="Nível inicial:").grid(row=2, column=0, padx=10, pady=5)
 entry_ni = tk.Entry(root)
-entry_ni.grid(row=1, column=1)
+entry_ni.grid(row=2, column=1)
 
-tk.Label(root, text="Nível final:").grid(row=2, column=0, padx=10, pady=5)
+tk.Label(root, text="Nível final:").grid(row=3, column=0, padx=10, pady=5)
 entry_nf = tk.Entry(root)
-entry_nf.grid(row=2, column=1)
+entry_nf.grid(row=3, column=1)
 
-tk.Label(root, text="Probabilidade (a <= x <= b)").grid(row=3, column=0, padx=5, pady=2)
+tk.Label(root, text="Probabilidade (a <= x <= b)").grid(row=4, column=0, padx=5, pady=2)
 
-tk.Label(root, text="a:").grid(row=4, column=0, padx=10, pady=5)
+tk.Label(root, text="a:").grid(row=5, column=0, padx=10, pady=5)
 entry_a = tk.Entry(root)
-entry_a.grid(row=4, column=1)
+entry_a.grid(row=5, column=1)
 
-tk.Label(root, text="b:").grid(row=5, column=0, padx=10, pady=5)    
+tk.Label(root, text="b:").grid(row=6, column=0, padx=10, pady=5)    
 entry_b = tk.Entry(root)
-entry_b.grid(row=5, column=1)
+entry_b.grid(row=6, column=1)
 
 massa = tk.StringVar(value="me")
-tk.Label(root, text="Massa (m):").grid(row=6, column=0, padx=10, pady=5)
-tk.Radiobutton(root, text="Massa do próton", variable=massa, value="mp").grid(row=6, column=1)
-tk.Radiobutton(root, text="Massa do elétron", variable=massa, value="me").grid(row=7, column=1)
+tk.Label(root, text="Massa (m):").grid(row=7, column=0, padx=10, pady=5)
+tk.Radiobutton(root, text="Massa do próton", variable=massa, value="mp").grid(row=7, column=1)
+tk.Radiobutton(root, text="Massa do elétron", variable=massa, value="me").grid(row=8, column=1)
 
-tk.Button(root, text="Calcular", command=0).grid(row=8, column=0, columnspan=2, pady=10)
-tk.Button(root, text="Gráficos", command=0).grid(row=8, column=1, columnspan=2, pady=10)
-tk.Button(root, text="Simulação", command=0).grid(row=9, column=0, columnspan=2, pady=10)
-tk.Button(root, text="Sobre", command=0).grid(row=9, column=1, columnspan=2, pady=10)
+tk.Button(root, text="Calcular", command=0).grid(row=9, column=0, columnspan=2, padx=10, pady=10)
+tk.Button(root, text="Gráficos", command=0).grid(row=9, column=1, columnspan=2, pady=10)
+tk.Button(root, text="Simulação", command=0).grid(row=10, column=0, columnspan=2, pady=10)
+tk.Button(root, text="Sobre", command=0).grid(row=10, column=1, columnspan=2, pady=10)
 
 
 root.mainloop()
